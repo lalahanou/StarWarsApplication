4d95d74e62a75ccc6555ddc88a9a631428dca59d
Hana <bahloul.hana@gmail.com>
2023-09-23 20:21:11 +0100

    Update gradle

--- app/src/main/java/com/example/starwarsapplication/MainActivity.kt
+++ app/src/main/java/com/example/starwarsapplication/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.starwarsapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private var errorSnackbar: Snackbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
